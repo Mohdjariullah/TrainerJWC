a79572619a1ed7943c2f839b4d127a4af84b2708
Jariullah <112340610+Mohdjariullah@users.noreply.github.com>
2025-05-20 07:44:40 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { FormProvider } from "@/context/FormContext";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         <FormProvider>
           <main className="h-screen w-screen">{children}</main>
         </FormProvider>
