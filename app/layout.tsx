a06390146c8ddf8cdb3714dc450af9a86ad019a5
Mohdjariullah <112340610+Mohdjariullah@users.noreply.github.com>
2025-05-29 04:55:04 +0530

    adding clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="bg-black">
-      <head>
+    <html lang="en" className="bg-black">      <head>
+        <script type="text/javascript">{`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "rqueo5xfod");
+        `}</script>
         <style>{`
           body {
             margin: 0;
@@ -45,8 +51,7 @@ export default function RootLayout({
             overflow-x: hidden;
           }
         `}</style>
-      </head>
-      <body className={`${inter.className} bg-black overflow-x-hidden`}>
+      </head>      <body className={`${inter.className} bg-black overflow-x-hidden`}>
         <CommentInjector />
         <FormProvider>
          
